ae11d6cf8b8d35eb8b7513c790ab9f5046802eb0
Daniel silva do nascimento <daniel.nascimento.1@academico.ifpb.edu.br>
2025-04-29 11:33:20 -0300

    Lista de usuarios

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
 import { Text, View } from "react-native";
 
+const usuarios = [
+ { 
+  id: 1,
+  nome: "Andre",
+  cargo: "Professor"
+ },
+ {
+  id: 2,
+  nome: "Cicinho cabeça",
+  cargo: "aluno"
+ } 
+]
+
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>daniel.</Text>
+      style={style.container}
+     >
+      {
+        usuarios.map( item => (
+         <View style={StylePropertyMap.itemLista}> 
+          <Text Style={{ fontSize: 20 }}>{item.nome}</Text>
+         </View> 
+        ))
+      } 
     </View>
   );
 }
